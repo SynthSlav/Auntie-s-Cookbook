8aead76bd768696d2532703215a23dd13b4a1dbd
SynthSlav <ljupcebozinovski@gmail.com>
2025-07-17 18:06:14 +0200

    registered models in the admin page

--- recipebook/settings.py
+++ recipebook/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_summernote",  # For rich text editing
     "recipes",  # First app for the recipebook project
 ]
 
--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient
 
 # Register your models here.
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "difficulty", "meal_type", "created_at")
+    list_filter = ("difficulty", "meal_type", "dietary_restrictions")
+    search_fields = ("title", "description", "ingredients__ingredient_name")
+    prepopulated_fields = {"slug": ("title",)}
+
+
+# @admin.register(Ingredient)
+# class IngredientAdmin(admin.ModelAdmin):
+#     list_display = ("ingredient_name", "quantity", "unit", "recipe")
+#     list_filter = ("unit",)
+#     search_fields = ("ingredient_name",)
